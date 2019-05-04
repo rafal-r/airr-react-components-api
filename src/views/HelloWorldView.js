59978230e23fa841cd6032a5e7d2af1ffca7d9dd
Rafal Rajtar <rafal.rajtar@consultant.volvo.com>
2019-05-04 17:48:07 +0200

    ViewsAPI dev.

---
+++ src/views/HelloWorldView.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ViewWrapper } from "airr-react";
+
+export const viewName = "hello-world-view";
+
+export default class HelloWorld extends ViewWrapper {
+	content() {
+		const content =
+			typeof this.props.render === "function"
+				? this.props.render()
+				: typeof this.props.children === "function"
+				? this.props.children()
+				: this.props.children;
+
+		return <div className={viewName}>{content ? content : "What up!"}</div>;
+	}
+}
