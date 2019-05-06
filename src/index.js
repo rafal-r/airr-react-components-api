83d5e12fb868158d3cf3e457ed243e0a5bb3cb9c
Rafal Rajtar <rafal.rajtar@consultant.volvo.com>
2019-05-06 21:58:52 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import CloseSidepanel from "./Scene/SidepanelAPI/CloseSidepanel";
-import GoToAnotherView from "./Scene/ViewsAPI/GoToAnotherView";
+// import GoToAnotherView from "./Scene/ViewsAPI/GoToAnotherView";
 
 const rootElement = document.getElementById("root");
-// ReactDOM.render(<div>Airr React Components API</div>, rootElement);
+ReactDOM.render(<div>Airr React Components API</div>, rootElement);
 // ReactDOM.render(<CloseSidepanel />, rootElement);
-ReactDOM.render(<GoToAnotherView />, rootElement);
+// ReactDOM.render(<GoToAnotherView />, rootElement);
